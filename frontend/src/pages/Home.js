8c29ac42448ebbc97f86113058210108ed209870
Sarahqtw <Sarahqtw@hotmail.com>
2022-03-21 16:05:27 +0800

    made manual add subscription into a form modal

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Avatar, Box, Button } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -8,10 +8,7 @@ import { GoogleLogout } from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import NewSubscriptionForm from '../components/NewSubscriptionForm';
 import SubscriptionsList from '../components/SubscriptionsList';
-import axios from 'axios';
-import applyCaseMiddleware from 'axios-case-converter';
-import { SubscriptionsContext } from '../context/Subscriptions.context';
-
+import FormModal from '../components/FormModal';
 const Home = () => {
 	const clientID = clientGoogle.web.client_id;
 	const navigate = useNavigate();
@@ -23,10 +20,13 @@ const Home = () => {
 		navigate('/');
 		setUserInfo({ name: 'Anonymous Badger', photoUrl: '' });
 	};
-	// useEffect(() => {
-	// 	getUserInfo();
-	// 	console.log('getting user info');
-	// }, []);
+	const [ modalOpen, setModalOpen ] = useState(false);
+	const handleOpenModal = () => {
+		setModalOpen(true);
+	};
+	const handleCloseModal = () => {
+		setModalOpen(false);
+	};
 	const handleNewScan = async () => {
 		navigate('/scan_results');
 	};
@@ -46,14 +46,14 @@ const Home = () => {
 						<Typography variant="h4">Welcome back, {userInfo.name}.</Typography>
 					</Box>
 					<GoogleLogout clientId={clientID} buttonText="Logout" onLogoutSuccess={logout} />
-					<NewSubscriptionForm />
-					<Button variant="contained" color="secondary" endIcon={<AddCircleIcon />}>
+					<Button variant="contained" onClick={handleOpenModal} color="secondary" endIcon={<AddCircleIcon />}>
 						ADD NEW SUBSCRIPTION
 					</Button>
 					<Button variant="contained" color="success" endIcon={<AddCircleIcon />} onClick={handleNewScan}>
 						SYNC
 					</Button>
 					<SubscriptionsList />
+					<FormModal modalOpen={modalOpen} handleCloseModal={handleCloseModal} />
 				</React.Fragment>
 			) : (
 				<Link to="/">Please log in to view this page</Link>
